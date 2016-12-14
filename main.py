7bcd236651941d2ec21ba0fccb04f72ca12327a0
Ignasi93 <i.salvador.llobet93@gmail.com>
2016-12-14 09:25:22 +0100

    Tarea 2 completa, ya valida todo

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import webapp2
+import os
 from google.appengine.ext.webapp \
 	import template
 
@@ -21,9 +22,49 @@ class MainHandler ( webapp2.RequestHandler ) :
 	def get ( self ) :
 		self.response.out.write ( template.render ( 'main_ca.html', {} ) )
 
+class RegisterMainHandler ( webapp2.RequestHandler ) :
+	def get ( self ) :
+		self.response.out.write ( template.render ( 'register_ca.html', {} ) )
+
+	def post ( self ) :
+		self.response.out.write ( template.render ( 'register_ca.html', {} ) )
+		self.response.write ( '<h2>Formulari rebut</h2>' )
+		self.response.write ( "Nom: %s <br>" % self.request.get ( 'username' ) )
+
+class RegisterEnglishHandler ( webapp2.RequestHandler ) :
+	def get ( self ) :
+		self.response.out.write ( template.render ( 'register_en.html', {} ) )
+
+	def post ( self ) :
+		self.response.out.write ( template.render ( 'register_en.html', {} ) )
+		self.response.write ( '<h2>Form received</h2>' )
+		self.response.write ( "Name: %s <br>" % self.request.get ( 'username' ) )
+
+class RegisterSpanishHandler ( webapp2.RequestHandler ) :
+	def get ( self ) :
+		self.response.out.write ( template.render ( 'register_es.html', {} ) )
+
+	def post ( self ) :
+		self.response.out.write ( template.render ( 'register_es.html', {} ) )
+		self.response.write ( '<h2>Formulario recibido</h2>' )
+		self.response.write ( "Nombre: %s <br>" % self.request.get ( 'username' ) )
+
+class RegisterEuskaraHandler ( webapp2.RequestHandler ) :
+	def get ( self ) :
+		self.response.out.write ( template.render ( 'register_eu.html', {} ) )
+
+	def post ( self ) :
+		self.response.out.write ( template.render ( 'register_eu.html', {} ) )
+		self.response.write ( '<h2>Formularioa ondo jaso da</h2>' )
+		self.response.write ( "Izena: %s <br>" % self.request.get ( 'username' ) )
+
 app = webapp2.WSGIApplication ( [
 	( '/', MainHandler ),
 	( '/en', EnglishHandler ),
 	( '/es', SpanishHandler ),
-	( '/eu', EuskaraHandler )
+	( '/eu', EuskaraHandler ),
+	( '/registre', RegisterMainHandler ),
+	( '/en/registre', RegisterEnglishHandler ),
+	( '/es/registre', RegisterSpanishHandler ),
+	( '/eu/registre', RegisterEuskaraHandler )
 ], debug = True)
