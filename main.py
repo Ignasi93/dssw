ebf031e390eaf812f9746a48f2d2a2d1dab85ac1
Ignasi93 <i.salvador.llobet93@gmail.com>
2016-12-14 12:22:00 +0100

    Tarea 3 en proceso

--- main.py
+++ main.py
@@ -3,8 +3,20 @@
 
 import webapp2
 import os
+import urllib
 from google.appengine.ext.webapp \
 	import template
+from google.appengine.api import images
+from google.appengine.ext import ndb
+
+class usuari ( ndb.Model ) :
+	nom = ndb.StringProperty ( required = True )
+	contrasenya = ndb.StringProperty ( required = True )
+	correu = ndb.StringProperty ( required = True )
+	data = ndb.DateTimeProperty ( auto_now_add = True )
+	imatge = ndb.BlobProperty ()
+
+#**********************************************************************************
 
 class EnglishHandler ( webapp2.RequestHandler ) :
 	def get ( self ) :
@@ -22,32 +34,134 @@ class MainHandler ( webapp2.RequestHandler ) :
 	def get ( self ) :
 		self.response.out.write ( template.render ( 'main_ca.html', {} ) )
 
+#**********************************************************************************
+
+USERNAME_RE = re.compile ( r"^[a-zA-Z0-9]{3,20}" )
+PASSWORD_RE = re.compile ( r"^.{6,15}" )
+EMAIL_RE = re.compile ( r"^([a-zA-Z0-9_.+-]) + \@ ( ( [a-zA-Z0-9] ) + \. ) + ( [a-zA-Z0-9] ) + $" )
+
+# 0 = username; 1 = password; 2 = email
+def validate ( field, num ) :
+	if num == 0 :
+		return USERNAME_RE.match ( username )
+	elif num == 1 :
+		return PASSWORD_RE.match ( password )
+	elif num == 2:
+		return EMAIL_RE.match ( email )
+
+def htmlValues ( un, pw, rpw, em, eun, epw, erpw, eem ) :
+	return {
+		'username': un,
+		'password': pw,
+		'repeatPassword': rpw,
+		'email': em,
+		'errorUN': eun,
+		'errorPW': epw,
+		'errorRPW': erpw,
+		'errorEM': eem
+	}
+
 class RegisterMainHandler ( webapp2.RequestHandler ) :
 	def get ( self ) :
 		self.response.out.write ( template.render ( 'register_ca.html', {} ) )
 
 	def post ( self ) :
-		self.response.out.write ( template.render ( 'register_ca.html', {} ) )
-		self.response.write ( '<h2>Formulari rebut</h2>' )
-		self.response.write ( "Nom: %s <br>" % self.request.get ( 'username' ) )
+		username = self.request.get ( 'username' )
+		password = self.request.get ( 'password' )
+		repeatPassword = self.request.get ( 'repeatPassword' )
+		email = self.request.get ( 'email' )
+		errorUN, errorPW, errorRPW, errorEM = ''
+		error = False
+
+		if not validate ( username, 0 ) :
+			error = True
+			errorUN = "El nom d'usuari no pot contindre nombres o s&iacute;mbols"
+		if not validate ( password, 1 ) :
+			error = True
+			errorPW = "La contrasenya ha de ser entre 6 i 15 caracters de llarg&agrave;ria i sense s&iacute;mbols"
+		if repeatPassword != password :
+			error = True
+			errorRPW = "Les contrasenyes han de ser iguals"
+		if not validate ( email, 2 ) :
+			error = True
+			errorEM = "No &eacute;s un correu v&agrave;lid"
+
+		if not error:
+			self.response.out.write ( template.render ( 'register_ca.html', {} ) )
+			self.response.write ( '<h2>Formulari rebut</h2>' )
+			self.response.write ( "Hola: %s <br>" % self.request.get ( 'username' ) )
+		else :
+			values = htmlValues ( username, password, repeatPassword, email, errorUN, errorPW, errorRPW, errorEM )
+			self.response.out.write ( template.render ( 'register_ca.html', values ) )
+			self.response.write ( '<h6>ERROR</h6>' )
 
 class RegisterEnglishHandler ( webapp2.RequestHandler ) :
 	def get ( self ) :
 		self.response.out.write ( template.render ( 'register_en.html', {} ) )
 
 	def post ( self ) :
-		self.response.out.write ( template.render ( 'register_en.html', {} ) )
-		self.response.write ( '<h2>Form received</h2>' )
-		self.response.write ( "Name: %s <br>" % self.request.get ( 'username' ) )
+		username = self.request.get ( 'username' )
+		password = self.request.get ( 'password' )
+		repeatPassword = self.request.get ( 'repeatPassword' )
+		email = self.request.get ( 'email' )
+		errorUN, errorPW, errorRPW, errorEM = ''
+		error = False
+
+		if not validate ( username, 0 ) :
+			error = True
+			errorUN = "Username can not contain numbers or symbols"
+		if not validate ( password, 1 ) :
+			error = True
+			errorPW = "Password length must be from 6 to 15 characters and can not contain symbols"
+		if repeatPassword != password :
+			error = True
+			errorRPW = "Passwords must be equals"
+		if not validate ( email, 2 ) :
+			error = True
+			errorEM = "Not an email"
+
+		if not error:
+			self.response.out.write ( template.render ( 'register_en.html', {} ) )
+			self.response.write ( '<h2>Form received</h2>' )
+			self.response.write ( "Hello: %s <br>" % self.request.get ( 'username' ) )
+		else :
+			values = htmlValues ( username, password, repeatPassword, email, errorUN, errorPW, errorRPW, errorEM )
+			self.response.out.write ( template.render ( 'register_en.html', values ) )
+			self.response.write ( '<h6>ERROR</h6>' )
 
 class RegisterSpanishHandler ( webapp2.RequestHandler ) :
 	def get ( self ) :
 		self.response.out.write ( template.render ( 'register_es.html', {} ) )
 
 	def post ( self ) :
-		self.response.out.write ( template.render ( 'register_es.html', {} ) )
-		self.response.write ( '<h2>Formulario recibido</h2>' )
-		self.response.write ( "Nombre: %s <br>" % self.request.get ( 'username' ) )
+		username = self.request.get ( 'username' )
+		password = self.request.get ( 'password' )
+		repeatPassword = self.request.get ( 'repeatPassword' )
+		email = self.request.get ( 'email' )
+		errorUN, errorPW, errorRPW, errorEM = ''
+		error = False
+
+		if not validate ( username, 0 ) :
+			error = True
+			errorUN = "El nombre de usuario no puede contener n&uacute;meros o s&iacute;mbolos"
+		if not validate ( password, 1 ) :
+			error = True
+			errorPW = "La contrase&ntilde;a tiene que ser de entre 6 y 15 car&aacute;cteres de longitud y sin s&iacute;mbolos"
+		if repeatPassword != password :
+			error = True
+			errorRPW = "Las contrase&ntilde;as tienen que ser iguales"
+		if not validate ( email, 2 ) :
+			error = True
+			errorEM = "No es un correu v&aacute;lido"
+
+		if not error:
+			self.response.out.write ( template.render ( 'register_es.html', {} ) )
+			self.response.write ( '<h2>Formulario recibido</h2>' )
+			self.response.write ( "Hola: %s <br>" % self.request.get ( 'username' ) )
+		else :
+			values = htmlValues ( username, password, repeatPassword, email, errorUN, errorPW, errorRPW, errorEM )
+			self.response.out.write ( template.render ( 'register_es.html', values ) )
+			self.response.write ( '<h6>ERROR</h6>' )
 
 class RegisterEuskaraHandler ( webapp2.RequestHandler ) :
 	def get ( self ) :
@@ -55,8 +169,113 @@ class RegisterEuskaraHandler ( webapp2.RequestHandler ) :
 
 	def post ( self ) :
 		self.response.out.write ( template.render ( 'register_eu.html', {} ) )
+
 		self.response.write ( '<h2>Formularioa ondo jaso da</h2>' )
-		self.response.write ( "Izena: %s <br>" % self.request.get ( 'username' ) )
+		self.response.write ( "Kaixo: %s <br>" % self.request.get ( 'username' ) )
+
+#**********************************************************************************
+
+class UsersMainHandler ( webapp2.RequestHandler ) :
+	def get ( self ) :
+		usuaris = usuari.query ()
+		self.response.write ( '<h1>Usuaris registrats</h1>' )
+		self.response.write ( '<link rel="stylesheet" href="/styles/table.css">' )
+		self.response.write ( '''
+			<table>
+				<tr>
+					<th>Nom</th>
+					<th>Correu</th>
+					<th>Registrat</th>
+					<th>Avatar</th>
+				</tr>
+		''' )
+		for current in usuaris.fetch () :
+			self.response.write ( ''' 
+				<tr>
+					<td>''' + current.nom + '''</td>
+					<td>''' + current.correu + '''</td>
+					<td>''' + std ( current.data ) + '''</td>
+					<td>''' )
+			self.response.write ( '<div><img src="/img?img_id=%s"></img>' % current.key.urlsafe () )
+			self.response.write ( '''</div></td></tr>''' )
+		self.response.write ( '''</table>''' )
+
+class UsersEnglishHandler ( webapp2.RequestHandler ) :
+	def get ( self ) :
+		usuaris = usuari.query ()
+		self.response.write ( '<h1>Registered users</h1>' )
+		self.response.write ( '<link rel="stylesheet" href="/styles/table.css">' )
+		self.response.write ( '''
+			<table>
+				<tr>
+					<th>Name</th>
+					<th>Email</th>
+					<th>Registered</th>
+					<th>Avatar</th>
+				</tr>
+		''' )
+		for current in usuaris.fetch () :
+			self.response.write ( ''' 
+				<tr>
+					<td>''' + current.nom + '''</td>
+					<td>''' + current.correu + '''</td>
+					<td>''' + std ( current.data ) + '''</td>
+					<td>''' )
+			self.response.write ( '<div><img src="/img?img_id=%s"></img>' % current.key.urlsafe () )
+			self.response.write ( '''</div></td></tr>''' )
+		self.response.write ( '''</table>''' )
+
+class UsersSpanishHandler ( webapp2.RequestHandler ) :
+	def get ( self ) :
+		usuaris = usuari.query ()
+		self.response.write ( '<h1>Usuarios registrados</h1>' )
+		self.response.write ( '<link rel="stylesheet" href="/styles/table.css">' )
+		self.response.write ( '''
+			<table>
+				<tr>
+					<th>Nombre</th>
+					<th>Correo</th>
+					<th>Registrado</th>
+					<th>Avatar</th>
+				</tr>
+		''' )
+		for current in usuaris.fetch () :
+			self.response.write ( ''' 
+				<tr>
+					<td>''' + current.nom + '''</td>
+					<td>''' + current.correu + '''</td>
+					<td>''' + std ( current.data ) + '''</td>
+					<td>''' )
+			self.response.write ( '<div><img src="/img?img_id=%s"></img>' % current.key.urlsafe () )
+			self.response.write ( '''</div></td></tr>''' )
+		self.response.write ( '''</table>''' )
+
+class UsersEuskaraHandler ( webapp2.RequestHandler ) :
+	def get ( self ) :
+		usuaris = usuari.query ()
+		self.response.write ( '<h1>Erabiltzaile erregistratuak</h1>' )
+		self.response.write ( '<link rel="stylesheet" href="/styles/table.css">' )
+		self.response.write ( '''
+			<table>
+				<tr>
+					<th>Izena</th>
+					<th>Email</th>
+					<th>Registrat</th>
+					<th>Avatar</th>
+				</tr>
+		''' )
+		for current in usuaris.fetch () :
+			self.response.write ( ''' 
+				<tr>
+					<td>''' + current.nom + '''</td>
+					<td>''' + current.correu + '''</td>
+					<td>''' + std ( current.data ) + '''</td>
+					<td>''' )
+			self.response.write ( '<div><img src="/img?img_id=%s"></img>' % current.key.urlsafe () )
+			self.response.write ( '''</div></td></tr>''' )
+		self.response.write ( '''</table>''' )
+
+#**********************************************************************************
 
 app = webapp2.WSGIApplication ( [
 	( '/', MainHandler ),
@@ -66,5 +285,9 @@ app = webapp2.WSGIApplication ( [
 	( '/registre', RegisterMainHandler ),
 	( '/en/registre', RegisterEnglishHandler ),
 	( '/es/registre', RegisterSpanishHandler ),
-	( '/eu/registre', RegisterEuskaraHandler )
+	( '/eu/registre', RegisterEuskaraHandler ),
+	( '/usuaris', UsersMainHandler ),
+	( '/en/usuaris', UsersEnglishHandler ),
+	( '/es/usuaris', UsersSpanishHandler ),
+	( '/en/usuaris', UsersEuskaraHandler ),
 ], debug = True)
